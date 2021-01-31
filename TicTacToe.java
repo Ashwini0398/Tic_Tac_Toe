e7439b295300668b9223522adc17db707bce54e4
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-31 22:29:10 +0530

    ADDED: Fresh start and display board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,7 +1,32 @@
-Public class TicTacToe
-{
-	public static void main(String Args[])
-	{
-		System.out.println("Welcome to the tic tac toe game");
-	}
+public class TicTakToe {
+
+    char[][] board = new char[3][3];
+
+    public static void display(char board[][])
+    {
+        for (int i=0; i<3; i++)
+        {
+            for(int j=0; j<3; j++){
+				
+            System.out.println(board[i][j] +"  ");
+			}
+			System.out.println();
+        }
+		
+    }
+
+
+    public static void main(String args[])
+    {
+        TicTakToe game= new TicTakToe();
+        int counter=0;
+        for (int i=0; i<3; i++)
+        {
+            for(int j=0; j<3; j++){
+                game.board[i][j]=Character.forDigit(counter++,10);
+            }
+
+        }
+        display(game.board);
+    }
 }
